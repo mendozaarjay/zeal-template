660e5257ee69e8811d8b59c7c9ab804d6a42ce34
mendozaarjay <mendozaarjay.05@gmail.com>
2020-03-15 11:02:35 +0800

    Zeal Template Initialize

---
+++ Zeal/Zeal/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Zeal.Models;
+
+namespace Zeal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult _LogoHeader()
+        {
+            return PartialView();
+        }
+        public IActionResult _Header()
+        {
+            return PartialView();
+        }
+        public IActionResult _SideBar()
+        {
+            return PartialView();
+        }
+        public IActionResult Login()
+        {
+            return PartialView();
+        }
+    }
+}
